f0340f0c0b4dc35c47912404b92268bc925bb904
Santiago15JM <113864007+Santiago15JM@users.noreply.github.com>
2022-09-29 20:51:27 -0500

    Update python.py

--- python.py
+++ python.py
@@ -1,1 +1,49 @@
+# Ej1
 print("Hello World")
+
+# Ej2 
+x = 35e3
+y = 12E4
+z = -87.7e100
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+# EJ3 Formatear un texto
+age = 36
+txt = "My name is John, and I am {}"
+print(txt.format(age))
+
+# EJ4
+tuple1 = ("abc", 34, True, 40, "male")
+
+# EJ5 Desempacar una tupla
+fruits = ("apple", "banana", "cherry")
+
+(green, yellow, red) = fruits
+
+print(green)
+print(yellow)
+print(red)
+
+# EJ6 Unir dos sets de tipos distintos
+set1 = {"a", "b" , "c"}
+set2 = {1, 2, 3}
+
+set3 = set1.union(set2)
+print(set3)
+
+# EJ7 Diccionario de varios tipos
+thisdict = {
+  "brand": "Ford",
+  "electric": False,
+  "year": 1964,
+  "colors": ["red", "white", "blue"]
+}
+# Cambiar un valor en el diccionario
+thisdict["year"] = 2018
+
+# EJ8 
+
+
